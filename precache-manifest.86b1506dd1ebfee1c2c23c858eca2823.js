3047870dd9968e6a91aaa0380d3b40c4832849b8
tang263475963 <tang263475963@gmail.com>
2020-07-23 12:42:31 +0700

    Updates

---
+++ precache-manifest.86b1506dd1ebfee1c2c23c858eca2823.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b530c6572c823f9eaf69c69cfa8b5397",
+    "url": "/basic-react2/index.html"
+  },
+  {
+    "revision": "328d091d351ce9009690",
+    "url": "/basic-react2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "66950332fcb6b8755ed5",
+    "url": "/basic-react2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "328d091d351ce9009690",
+    "url": "/basic-react2/static/js/2.ea5ef707.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/basic-react2/static/js/2.ea5ef707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66950332fcb6b8755ed5",
+    "url": "/basic-react2/static/js/main.f9639e15.chunk.js"
+  },
+  {
+    "revision": "647aa9bc33bf8e56b053",
+    "url": "/basic-react2/static/js/runtime-main.397bb5e6.js"
+  }
+]);
